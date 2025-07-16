aded74578239769e158621c694990c0eca767d88
wael <waelnouira2@gmail.com>
2025-07-16 22:42:03 +0100

    Update api.js

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // Configuration de base pour l'API
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://padelvar-backend.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
